b8ed807e2b8e994d5725c9e86726c4534f37e878
aaron <aaron.simonson491@gmail.com>
2023-04-25 22:16:59 -0400

    triangle solution

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -1,10 +1,35 @@
 def equilateral(sides):
-    pass
+    """
+
+    :param sides:
+    :return:
+    """
+    return sides[0] == sides[1] and sides[1] == sides[2] and isTriangle(sides)
 
 
 def isosceles(sides):
-    pass
+    """
+
+    :param sides:
+    :return:
+    """
+    return (sides[0] == sides[1] or sides[1] == sides[2] or sides[0] == sides[2]) and isTriangle(sides)
 
 
 def scalene(sides):
-    pass
+    """
+
+    :param sides:
+    :return:
+    """
+    return sides[0] != sides[1] and sides[1] != sides[2] and sides[0] != sides[2] and isTriangle(sides)
+
+
+def isTriangle(sides):
+    """
+
+    :param sides:
+    :return:
+    """
+    return sides[0] > 0 and sides[1] > 0 and sides[2] > 0 and sides[0] + sides[1] >= sides[2] and sides[1] + sides[2] >= \
+           sides[0] and sides[0] + sides[2] >= sides[1]
