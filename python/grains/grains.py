47b6a372d1fbd3b3ea1308bfd79b56223029b945
aaron <aaron.simonson491@gmail.com>
2023-04-25 21:24:05 -0400

    grains solution

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,6 +1,17 @@
 def square(number):
-    pass
+    """
+
+    :param number:
+    :return:
+    """
+    if number > 64 or number < 1:
+        raise ValueError("square must be between 1 and 64")
+    else:
+        return 2 ** (number-1)
 
 
 def total():
-    pass
+    counter = 0
+    for x in range(1, 65):
+        counter += square(x)
+    return counter
