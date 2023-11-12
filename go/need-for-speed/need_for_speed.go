72cb88b6ab5ebe56f0a6b68abe8c9a8ff7f803a2
Aaron Edward <aaron.simonson491@gmail.com>
2023-11-12 18:54:02 -0500

    need_for_speed solution

--- go/need-for-speed/need_for_speed.go
+++ go/need-for-speed/need_for_speed.go
@@ -16,19 +16,34 @@ func NewCar(speed, batteryDrain int) Car {
 }
 
 // TODO: define the 'Track' type struct
+type Track struct {
+	distance int
+}
 
 // NewTrack creates a new track
 func NewTrack(distance int) Track {
-	panic("Please implement the NewTrack function")
+	return Track{
+		distance: distance,
+	}
 }
 
 // Drive drives the car one time. If there is not enough battery to drive one more time,
 // the car will not move.
 func Drive(car Car) Car {
-	panic("Please implement the Drive function")
+	if car.battery >= car.batteryDrain {
+		car.battery -= car.batteryDrain
+		car.distance += car.speed
+		return car
+	}
+	return car
 }
 
 // CanFinish checks if a car is able to finish a certain track.
 func CanFinish(car Car, track Track) bool {
-	panic("Please implement the CanFinish function")
+	distanceMaximum := (car.battery / car.batteryDrain) * car.speed
+
+	if distanceMaximum < track.distance {
+		return false
+	}
+	return true
 }
