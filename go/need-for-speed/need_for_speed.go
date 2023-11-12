8a2d47067fc6780e802df5ff1f673344c41b0361
aaron <aaron.simonson491@gmail.com>
2023-11-12 16:00:36 -0500

    changes

--- go/need-for-speed/need_for_speed.go
+++ go/need-for-speed/need_for_speed.go
@@ -1,10 +1,18 @@
 package speed
 
 // TODO: define the 'Car' type struct
+type Car struct {
+	battery, batteryDrain, speed, distance int
+}
 
 // NewCar creates a new remote controlled car with full battery and given specifications.
 func NewCar(speed, batteryDrain int) Car {
-	panic("Please implement the NewCar function")
+	return Car{
+		battery:      100,
+		speed:        speed,
+		batteryDrain: batteryDrain,
+	}
+
 }
 
 // TODO: define the 'Track' type struct
