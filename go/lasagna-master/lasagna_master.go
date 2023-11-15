0c882cf7761cab7fbe1805788f61190f71a2a972
aaron <aaron.simonson491@gmail.com>
2023-11-15 00:45:30 -0500

    func PreparationTime defined

--- go/lasagna-master/lasagna_master.go
+++ go/lasagna-master/lasagna_master.go
@@ -1,6 +1,13 @@
 package lasagna
 
 // TODO: define the 'PreparationTime()' function
+func PreparationTime(layers []string, prepTime int) int {
+	if prepTime == 0 {
+		return (2 * len(layers))
+	} else {
+		return (prepTime * len(layers))
+	}
+}
 
 // TODO: define the 'Quantities()' function
 
@@ -11,8 +18,8 @@ package lasagna
 // Your first steps could be to read through the tasks, and create
 // these functions with their correct parameter lists and return types.
 // The function body only needs to contain `panic("")`.
-// 
+//
 // This will make the tests compile, but they will fail.
 // You can then implement the function logic one by one and see
-// an increasing number of tests passing as you implement more 
+// an increasing number of tests passing as you implement more
 // functionality.
