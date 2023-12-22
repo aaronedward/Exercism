b048e351a36b562403deeaa8548d1f0e8f7128ab
Aaron <aaron.simonson491@gmail.com>
2023-12-22 06:03:00 +0000

    NewElectionResult, DisplayResult, DecrementVotesOfCandidate implemented

--- go/election-day/election_day.go
+++ go/election-day/election_day.go
@@ -1,5 +1,7 @@
 package electionday
 
+import "fmt"
+
 // NewVoteCounter returns a new vote counter with
 // a given number of initial votes.
 func NewVoteCounter(initialVotes int) *int {
@@ -22,15 +24,16 @@ func IncrementVoteCount(counter *int, increment int) {
 
 // NewElectionResult creates a new election result.
 func NewElectionResult(candidateName string, votes int) *ElectionResult {
-	panic("Please implement the NewElectionResult() function")
+	return &ElectionResult{candidateName, votes}
 }
 
 // DisplayResult creates a message with the result to be displayed.
 func DisplayResult(result *ElectionResult) string {
-	panic("Please implement the DisplayResult() function")
+	//var p = &result
+	return result.Name + " (" + fmt.Sprintf("%d", result.Votes) + ")"
 }
 
 // DecrementVotesOfCandidate decrements by one the vote count of a candidate in a map.
 func DecrementVotesOfCandidate(results map[string]int, candidate string) {
-	panic("Please implement the DecrementVotesOfCandidate() function")
+	results[candidate] -= 1
 }
