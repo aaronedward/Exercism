c81ae2c086a359389122516bcc49d72d57332930
Aaron <aaron.simonson491@gmail.com>
2024-01-03 04:57:55 +0000

    functions declared

--- go/elons-toys/elons_toys.go
+++ go/elons-toys/elons_toys.go
@@ -1,18 +1,30 @@
 package elon
 
 // TODO: define the 'Drive()' method
+func (c Car) Drive() {
+	panic("")
+}
 
 // TODO: define the 'DisplayDistance() string' method
+func (c Car) DisplayDistance() string {
+	panic("")
+}
 
 // TODO: define the 'DisplayBattery() string' method
+func (c Car) DisplayBattery() string {
+	panic("")
+}
 
 // TODO: define the 'CanFinish(trackDistance int) bool' method
+func (c Car) CanFinish(trackDistance int) bool {
+	panic("")
+}
 
 // Your first steps could be to read through the tasks, and create
 // these functions with their correct parameter lists and return types.
 // The function body only needs to contain `panic("")`.
-// 
+//
 // This will make the tests compile, but they will fail.
 // You can then implement the function logic one by one and see
-// an increasing number of tests passing as you implement more 
+// an increasing number of tests passing as you implement more
 // functionality.
