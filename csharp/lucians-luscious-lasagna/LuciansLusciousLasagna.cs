de67418d7adce5bd0d43084ce45dc5616c31fa9a
Aaron <aaron.simonson491@gmail.com>
2024-01-07 07:01:08 +0000

    convert RemainingMinutesInOven to expression-bodied method

--- csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
+++ csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
@@ -1,10 +1,21 @@
 class Lasagna
 {
     // TODO: define the 'ExpectedMinutesInOven()' method
+    public int ExpectedMinutesInOven(){
+        return 40;
+    }
 
     // TODO: define the 'RemainingMinutesInOven()' method
+    public int RemainingMinutesInOven(int minutesCooked) => ExpectedMinutesInOven() - minutesCooked;
 
     // TODO: define the 'PreparationTimeInMinutes()' method
+    public int PreparationTimeInMinutes(int layers) {
+        var layerTime = 2;
+        return layers * layerTime;
+    }
 
     // TODO: define the 'ElapsedTimeInMinutes()' method
+    public int ElapsedTimeInMinutes(int layers, int minutesCooked) {
+        return PreparationTimeInMinutes(layers) + minutesCooked;
+    }
 }
