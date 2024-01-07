92fc2772e17e536d2a52d4ab8b7aee91cac7edf8
Aaron <aaron.simonson491@gmail.com>
2024-01-07 22:43:24 +0000

    SubstringAfter() impplemented

--- csharp/log-analysis/LogAnalysis.cs
+++ csharp/log-analysis/LogAnalysis.cs
@@ -3,6 +3,7 @@ using System;
 public static class LogAnalysis 
 {
     // TODO: define the 'SubstringAfter()' extension method on the `string` type
+    public static string SubstringAfter(this string str, string delim) => Substring(delim,str.Length-delim);
 
     // TODO: define the 'SubstringBetween()' extension method on the `string` type
     
