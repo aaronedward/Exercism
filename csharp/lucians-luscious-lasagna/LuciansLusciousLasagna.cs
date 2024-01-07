31b1776f51867fbd17bc153f503381ccd931ad02
Aaron <aaron.simonson491@gmail.com>
2024-01-07 07:03:32 +0000

    cleanup

--- csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
+++ csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
@@ -1,17 +1,13 @@
 class Lasagna
 {
-    // TODO: define the 'ExpectedMinutesInOven()' method
     public int ExpectedMinutesInOven() => 40;
 
-    // TODO: define the 'RemainingMinutesInOven()' method
     public int RemainingMinutesInOven(int minutesCooked) => ExpectedMinutesInOven() - minutesCooked;
 
-    // TODO: define the 'PreparationTimeInMinutes()' method
     public int PreparationTimeInMinutes(int layers) {
         var layerTime = 2;
         return layers * layerTime;
     }
 
-    // TODO: define the 'ElapsedTimeInMinutes()' method
     public int ElapsedTimeInMinutes(int layers, int minutesCooked) => PreparationTimeInMinutes(layers) + minutesCooked;
 }
