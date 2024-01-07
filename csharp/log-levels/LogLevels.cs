1c4095fbe81ad3c1a666679f62255c16e21d2281
Aaron <aaron.simonson491@gmail.com>
2024-01-07 19:10:45 +0000

    solution

--- csharp/log-levels/LogLevels.cs
+++ csharp/log-levels/LogLevels.cs
@@ -2,18 +2,32 @@ using System;
 
 static class LogLine
 {
+    public static string[] logLevels = {"[ERROR]:","[WARNING]:","[INFO]:"};
     public static string Message(string logLine)
     {
-        throw new NotImplementedException("Please implement the (static) LogLine.Message() method");
+        string cleanedLogLine ="";
+        foreach (string x in logLevels)
+        {
+            if (logLine.Contains(x))
+            {
+                cleanedLogLine = logLine.Replace(x,"").Trim();
+            }
+        }
+        return cleanedLogLine;
     }
 
     public static string LogLevel(string logLine)
     {
-        throw new NotImplementedException("Please implement the (static) LogLine.LogLevel() method");
+        if (logLine.Contains("ERROR"))
+        {
+            return "error";
+        } else if (logLine.Contains("WARNING")){
+            return "warning";
+        } else if (logLine.Contains("INFO")){
+            return "info";
+        } 
+        return "";
     }
 
-    public static string Reformat(string logLine)
-    {
-        throw new NotImplementedException("Please implement the (static) LogLine.Reformat() method");
-    }
+    public static string Reformat(string logLine) => $"{Message(logLine)} ({LogLevel(logLine)})";
 }
