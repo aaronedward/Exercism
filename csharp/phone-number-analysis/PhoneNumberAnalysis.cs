b50a3d16748bf17005b9204ae6ecea4ab1d4d512
Aaron Edward <aaron.simonson491@gmail.com>
2024-01-07 22:59:24 -0500

    convert Analyze method to expression-bodied method

--- csharp/phone-number-analysis/PhoneNumberAnalysis.cs
+++ csharp/phone-number-analysis/PhoneNumberAnalysis.cs
@@ -2,10 +2,7 @@ using System;
 
 public static class PhoneNumber
 {
-    public static (bool IsNewYork, bool IsFake, string LocalNumber) Analyze(string phoneNumber)
-    {
-        return (phoneNumber.Substring(0, 3).Contains("212"), phoneNumber.Substring(4, 3).Contains("555"), phoneNumber.Substring(8,4));
-    }
+    public static (bool IsNewYork, bool IsFake, string LocalNumber) Analyze(string phoneNumber) => (phoneNumber.Substring(0, 3).Contains("212"), phoneNumber.Substring(4, 3).Contains("555"), phoneNumber.Substring(8,4));
 
     public static bool IsFake((bool IsNewYork, bool IsFake, string LocalNumber) phoneNumberInfo) => phoneNumberInfo.IsFake;
 }
