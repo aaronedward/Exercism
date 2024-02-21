e26d4e57a3a58982a4169502ee314cb39c13052b
aaronedward <aaron.simonson491@gmail.com>
2024-02-20 21:49:01 -0500

    solution

--- csharp/roll-the-die/RollTheDie.cs
+++ csharp/roll-the-die/RollTheDie.cs
@@ -4,11 +4,15 @@ public class Player
 {
     public int RollDie()
     {
-        throw new NotImplementedException("Please implement the Player.RollDie() method");
+        //throw new NotImplementedException("Please implement the Player.RollDie() method");
+        Random random = new Random();
+        return random.Next(1,18);
     }
 
     public double GenerateSpellStrength()
     {
-        throw new NotImplementedException("Please implement the Player.GenerateSpellStrength() method");
+        //throw new NotImplementedException("Please implement the Player.GenerateSpellStrength() method");
+        Random random = new Random();
+        return random.NextDouble() * (99.99-0.0);
     }
 }
