3fa287ded2e4490c6045fdb9afc8cd0194305227
aaron <aaron.simonson491@gmail.com>
2024-04-07 18:35:36 -0400

    need-for-speed solution

--- csharp/need-for-speed/NeedForSpeed.cs
+++ csharp/need-for-speed/NeedForSpeed.cs
@@ -2,35 +2,48 @@ using System;
 
 class RemoteControlCar
 {
-    // TODO: define the constructor for the 'RemoteControlCar' class
-
-    public bool BatteryDrained()
-    {
-        throw new NotImplementedException("Please implement the RemoteControlCar.BatteryDrained() method");
+    private int battery=100, speed, distance=0, batteryDrain;
+    public RemoteControlCar(int speed, int batteryDrain){
+        this.speed = speed;
+        this.batteryDrain = batteryDrain;
     }
+
+    public bool BatteryDrained() => this.battery - batteryDrain < 0;
 
     public int DistanceDriven()
     {
-        throw new NotImplementedException("Please implement the RemoteControlCar.DistanceDriven() method");
+        return this.distance;
     }
 
     public void Drive()
     {
-        throw new NotImplementedException("Please implement the RemoteControlCar.Drive() method");
+        if (!BatteryDrained())
+        {
+            this.distance += speed;
+            this.battery -= batteryDrain;            
+        }
     }
 
     public static RemoteControlCar Nitro()
     {
-        throw new NotImplementedException("Please implement the (static) RemoteControlCar.Nitro() method");
+        return new RemoteControlCar(50,4);
     }
 }
 
 class RaceTrack
 {
-    // TODO: define the constructor for the 'RaceTrack' class
+    
+    private int distance;
+    public RaceTrack(int distance){
+        this.distance = distance;
+    }
 
     public bool TryFinishTrack(RemoteControlCar car)
     {
-        throw new NotImplementedException("Please implement the RaceTrack.TryFinishTrack() method");
+        while (!car.BatteryDrained())
+        {
+            car.Drive();
+        }
+        return car.DistanceDriven() >= distance;
     }
 }
